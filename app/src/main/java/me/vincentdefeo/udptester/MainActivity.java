c8cbd2c3e3a7ea5d7e7502d0145d2134fac0cb55
Vincent <ghzmdr@gmail.com>
2015-05-09 19:02:13 +0200

    Bugfixes and UI updates

--- app/src/main/java/me/vincentdefeo/udptester/MainActivity.java
+++ app/src/main/java/me/vincentdefeo/udptester/MainActivity.java
@@ -1,11 +1,18 @@
 package me.vincentdefeo.udptester;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.internal.view.menu.ActionMenuItemView;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -230,28 +237,67 @@ public class MainActivity extends AppCompatActivity
             if (payload != null) PacketClientTask.justConnected = false;
         } else statusText.setText("IDLE");
     }
-/*
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_info) {
+            //new InfoDialog(this).show();
+            final AlertDialog infoDialog = new AlertDialog.Builder(this)
+                    .setTitle("Info")
+                    .setMessage(R.string.info_text)
+
+
+                    .setPositiveButton("See Sources", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent i = new Intent(Intent.ACTION_VIEW);
+                            i.setData(Uri.parse("https://github.com/ghzmdr/UDP-Tester"));
+                            startActivity(i);
+                        }
+                    })
+                    .create();
+            infoDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "NEVERMIND", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    infoDialog.dismiss();
+                }
+            });
+            infoDialog.show();
+            ((TextView) infoDialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+
+        } else if (id == R.id.action_reset) {
+            serverHostET.setText("");
+            serverPortET.setText("");
+            localPortET.setText("");
+            messageET.setText("");
+            responseText.setText("");
+            /*
+            item.setIcon(R.drawable.ic_chevron_up_white_24dp);
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            item.setIcon(R.drawable.ic_delete_white_24dp);
+                        }
+                    },
+                    1000);
+            */
         }
 
         return super.onOptionsItemSelected(item);
     }
-*/
+
 }
